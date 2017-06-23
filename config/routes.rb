8ee26d3b51c4fa1cf00df6b9459138bcd4106560
Jim Coble <jim.coble@duke.edu>
2017-06-23 09:14:00 -0400

    Remove sidekiq.

--- config/routes.rb
+++ config/routes.rb
@@ -29,8 +29,5 @@ Rails.application.routes.draw do
     end
   end
 
-  require 'sidekiq/web'
-  mount Sidekiq::Web => '/sidekiq'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
